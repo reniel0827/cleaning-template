cff7d88590801f74e4232654c2aba1a66c870da8
reniel0827 <renielvillegas@gmail.com>
2025-04-05 05:06:18 +0800

    fridaycommit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Yeg Carpet Cleaning Services",
-    "email": "yeg.carpetclean@gmail.com",
-    "phoneForTel": "587-335-2755",
-    "phoneFormatted": "(587) 335-2755",
+    "name": "Cleaning Company", 
+    "email": "cleaningCompany@gmail.com",
+    "phoneForTel": "123-456-7890",
+    "phoneFormatted": "(123) 456-7890",
     "address": {
-        "lineOne": "10429 32a Ave NW",
+        "lineOne": "your address",
         "lineTwo": "",
-        "city": "Edmonton",
-        "state": "AB",
-        "zip": "T6J 4A7",
-        "country": "Canada",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "city": "your city",
+        "state": "your state",
+        "zip": "zipcode",
+        "country": "Country",
+        "mapLink": "paste your map link here"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/profile.php?id=61565531703551/",
+        "facebook": "https://www.facebook.com",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://yegcarpetcleaningservices.ca"
+    "domain": "https://cleaningCompany.ca"
 };
